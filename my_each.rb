c13f7f44c0b0b8479f59784fc174e909f9599743
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 01:06:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+counter = 0 
+while counter < collection.length 
+puts collection[conunter]
+counter += 1 
+end
+collection
 end
